0b22cab6b88c4c97b63c9405040563f431ec66b3
nastys <alisfquarto@live.it>
2018-05-21 19:22:00 +0200

    Update install-mf.sh

--- install-mf.sh
+++ install-mf.sh
@@ -6,5 +6,5 @@ $DIRECTORY/installcab.py "$1" mf_
 $DIRECTORY/installcab.py "$1" mfreadwrite
 $DIRECTORY/installcab.py "$1" wmadmod
 $DIRECTORY/installcab.py "$1" wmvdecod
-if [[ $1 == *x64* ]]; then windir=syswow64; else windir=system32; fi
+if [ -d "$WINEPREFIX/drive_c/windows/syswow64" ]; then windir=syswow64; else windir=system32; fi
 wget "http://www.telecharger-dll.fr/dll/m/mfplat.dll" -O"$WINEPREFIX/drive_c/windows/$windir/mfplat.dll"
